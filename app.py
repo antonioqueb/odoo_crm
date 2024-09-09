5a4b014410b5bb3dc4dab067710086d17f574f24
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-09-09 10:06:31 -0600

    Up

--- app.py
+++ app.py
@@ -156,14 +156,10 @@ def available_slots():
             ("20:00", "21:00"),
             ("21:00", "22:00"),
             ("22:00", "23:00"),
+            ("23:00", "00:00"),
+            ("00:00", "01:00"),
+            ("01:00", "02:00")
         ]
-        # working_hours = [
-        #     ("10:00", "11:00"),
-        #     ("13:00", "14:00"),
-        #     ("14:00", "15:00"),
-        #     ("15:00", "16:00"),
-        #     ("16:00", "17:00")
-        # ]
 
         # Buscar eventos en el calendario para la empresa específica en el rango de tiempo dado
         events = models.execute_kw(
