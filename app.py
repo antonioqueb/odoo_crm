b98f26772169c7738c1e7292b9cfdcd81e345b0b
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-09-10 17:10:48 -0600

    Up

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from flask import Flask
 from flask_cors import CORS
-from app.routes.opportunity import opportunity_blueprint
-from app.routes.calendar import calendar_blueprint
+from routes.opportunity import opportunity_blueprint
+from routes.calendar import calendar_blueprint
+
 
 def create_app():
     app = Flask(__name__)
