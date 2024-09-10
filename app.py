804eec83a4370c66068d087421cae2c45aa0d8a9
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-09-10 16:49:22 -0600

    Up

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Añadir la ruta principal del proyecto al sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from flask import Flask
 from flask_cors import CORS
 from app.routes.opportunity import opportunity_blueprint
@@ -7,7 +13,7 @@ def create_app():
     app = Flask(__name__)
     CORS(app, resources={r"/*": {"origins": "https://inventario-plus.gestpro.cloud"}})
 
-    # Registrar los blueprints para organizar las rutas
+    # Registrar los blueprints
     app.register_blueprint(opportunity_blueprint)
     app.register_blueprint(calendar_blueprint)
 
