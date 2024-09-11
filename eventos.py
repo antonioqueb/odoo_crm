1a1e5df0e6d51df37d01ea9286dd8e993c2ffc5f
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-09-10 19:45:25 -0600

    Up

--- eventos.py
+++ eventos.py
@@ -4,38 +4,25 @@ import pytz
 
 def get_events(models, db, uid, password, mexico_tz):
     try:
-        # Obtener los parámetros de la solicitud
         start_time, end_time, company_id = (request.args.get(k) for k in ['start_time', 'end_time', 'company_id'])
         if not all([start_time, end_time, company_id]):
             raise ValueError("Los parámetros start_time, end_time y company_id son obligatorios.")
         
-        # Convertir los tiempos a la zona horaria de México
         start_dt = mexico_tz.localize(datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%S'))
         end_dt = mexico_tz.localize(datetime.strptime(end_time, '%Y-%m-%dT%H:%M:%S'))
 
-        # Consultar los eventos en la base de datos de Odoo
         events = models.execute_kw(
             db, uid, password, 'calendar.event', 'search_read', [[
                 ('start', '<=', end_time), ('stop', '>=', start_time), ('company_id', '=', int(company_id))
-            ]], {'fields': ['start', 'stop']}
+            ]], {'fields': ['id', 'name', 'start', 'stop', 'company_id', 'user_id', 'partner_ids', 'description', 'allday', 'location']}
         )
 
-        # Formatear cada evento
-        formatted_events = []
         for event in events:
-            # Convertir las fechas a la zona horaria de México y al formato adecuado
             event_start_mx = pytz.utc.localize(datetime.strptime(event['start'], '%Y-%m-%d %H:%M:%S')).astimezone(mexico_tz)
             event_stop_mx = pytz.utc.localize(datetime.strptime(event['stop'], '%Y-%m-%d %H:%M:%S')).astimezone(mexico_tz)
-            
-            # Actualizar los valores en el formato correcto y agregarlos a la lista de eventos formateados
-            formatted_events.append({
-                'start': event_start_mx.strftime('%Y-%m-%d %H:%M:%S'),
-                'stop': event_stop_mx.strftime('%Y-%m-%d %H:%M:%S')
-            })
+            event.update({'start': event_start_mx.strftime('%Y-%m-%d %H:%M:%S'), 'stop': event_stop_mx.strftime('%Y-%m-%d %H:%M:%S')})
 
-        # Retornar la respuesta en formato JSON
-        return jsonify(formatted_events), 200
+        return jsonify({'status': 'success', 'events': events}), 200
 
     except Exception as e:
-        # Manejo de errores
         return jsonify({'status': 'error', 'message': str(e)}), 500
