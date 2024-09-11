88c4181dbd041483dc9c29888dd5466cf3a20fbf
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-09-10 18:26:41 -0600

    Up

--- utils/time_utils.py
+++ utils/time_utils.py
@@ -3,6 +3,14 @@ from datetime import datetime
 
 mexico_tz = pytz.timezone('America/Mexico_City')
 
-def local_to_utc(local_time_str, fmt='%Y-%m-%d %H:%M:%S'):
-    local_time = mexico_tz.localize(datetime.strptime(local_time_str, fmt))
+def local_to_utc(local_time_str):
+    try:
+        # Intentar con formato ISO 8601: 'YYYY-MM-DDTHH:MM:SS'
+        local_time = datetime.strptime(local_time_str, '%Y-%m-%dT%H:%M:%S')
+    except ValueError:
+        # Intentar con formato 'YYYY-MM-DD HH:MM:SS'
+        local_time = datetime.strptime(local_time_str, '%Y-%m-%d %H:%M:%S')
+    
+    # Localizar en la zona horaria de México y convertir a UTC
+    local_time = mexico_tz.localize(local_time)
     return local_time.astimezone(pytz.utc)
